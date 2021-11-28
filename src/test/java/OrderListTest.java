1fa6690fe1b1a555e9701f190e93096760d85a34
MartynovaAnna <93049645+MartynovaAnna@users.noreply.github.com>
2021-11-28 12:04:14 +0300

    Project Sprint_3

---
+++ src/test/java/OrderListTest.java
@@ -0,0 +1,34 @@
+import io.qameta.allure.Description;
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.RestAssured;
+import org.junit.Before;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.notNullValue;
+
+public class OrderListTest {
+
+    @Before
+    public void setUp() {
+        RestAssured.baseURI = "http://qa-scooter.praktikum-services.ru";
+    }
+
+
+    @Test
+    @DisplayName("Can get order list")
+    @Description("This is the getting order list test with console output")
+
+    public void canGetOrderList() {
+        OrderCreation orderCreation = OrderCreation.getOrderData();
+        given()
+                .header("Content-type", "application/json")
+                .and()
+                .body(orderCreation)
+                .when()
+                .post("/api/v1/orders");
+        given()
+                .get("/api/v1/orders")
+                .then().assertThat().body("orders", notNullValue());
+    }
+}
