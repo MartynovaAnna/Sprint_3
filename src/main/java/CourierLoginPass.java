2a955e0e652e7b2923f6ea9e88e1cba22b5fe2fc
MartynovaAnna <93049645+MartynovaAnna@users.noreply.github.com>
2021-11-29 22:27:52 +0300

    Project Sprint_3

---
+++ src/main/java/CourierLoginPass.java
@@ -0,0 +1,14 @@
+public class CourierLoginPass {
+
+    public final String login;
+    public final String password;
+
+    public CourierLoginPass(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public static CourierLoginPass from(Courier courier) {
+        return new CourierLoginPass(courier.login, courier.password);
+    }
+}
